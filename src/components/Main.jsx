56821482af30e72f3e0042c516d38601728b5192
1-Amin <azimovaminjon063@gmail.com>
2023-10-06 06:55:36 +0700

    Created main section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,6 +4,75 @@ function Main() {
   return (
     <div className="main">
       <h2>ОПИСАНИЕ ИГРЫ</h2>
+      <div className="opisanie opisanie1">
+        <img src="./images/Op1.png" />
+        <div className="opisanieText">
+          <h3>Приветствие</h3>
+          <p>
+            Создадим космический корабль, научимся отправлять и принимать
+            сообщения с земли (Ввод/вывод данных).
+          </p>
+          <button className="opisanieButton opisanieButton1">
+            БЕСПЛАТНЫЙ ДОСТУП
+          </button>
+        </div>
+      </div>
+      <div className="arrowbox arrowbox1">
+        <img src="./images/arrow1.svg" className="arrow arrow1" />
+      </div>
+
+      <div className="opisanie opisanie2">
+        <img src="./images/Op2.png" />
+        <div className="opisanieText">
+          <h3>Запоминаем имена</h3>
+          <p>Научимся управлять кораблем и запишем имя капитана корабля</p>
+          <button className="opisanieButton opisanieButton1">
+            БЕСПЛАТНЫЙ ДОСТУП
+          </button>
+        </div>
+      </div>
+      <div className="arrowbox arrowbox2">
+        <img src="./images/arrow1.svg" className="arrow arrow2" />
+      </div>
+
+      <div className="opisanie opisanie3">
+        <img src="./images/Op3.png" />
+        <div className="opisanieText">
+          <h3>Автопилот</h3>
+          <p>Научимся облетать астероиды и другие корабли (Условия).</p>
+          <button className="opisanieButton opisanieButton2">
+            ДОСТУП ПО ПОДПИСКЕ
+          </button>
+        </div>
+      </div>
+      <div className="arrowbox arrowbox3">
+        <img src="./images/arrow2.svg" className="arrow arrow3" />
+      </div>
+
+      <div className="opisanie opisanie4">
+        <img src="./images/Op4.png" />
+        <div className="opisanieText">
+          <h3>Автопилот +</h3>
+          <p>Усовершенствуем наш Автопилот с помощью цикла (Цикл).</p>
+          <button className="opisanieButton opisanieButton2">
+            ДОСТУП ПО ПОДПИСКЕ
+          </button>
+        </div>
+      </div>
+      <div className="arrowbox arrowbox4">
+        <img src="./images/arrow2.svg" className="arrow arrow4" />
+      </div>
+
+      <div className="opisanie opisanie5">
+        <img src="./images/Op5.png" />
+        <div className="opisanieText">
+          <h3>Функции</h3>
+          <p>Подключим новые модули к нашему кораблю (Функции).</p>
+          <button className="opisanieButton opisanieButton2">
+            ДОСТУП ПО ПОДПИСКЕ
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,26 @@
 import React from "react";
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 
+function Navbar() {
+  return (
+    <nav>
+      <div className="navbar">
+        <a>Описание игры</a>
+        <a>Уровни</a>
+        <a>Тарифы</a>
+        <a>Отзывы</a>
+        <a>Партнеры</a>
+        <a>Профиль</a>
+        <a>Игра</a>
+        <div className="burger">
+        <MenuRoundedIcon sx={{fontSize: 30}}/>
+      </div>
+      </div>
 
-function Navbar(){
-    return(
-        <div className="navbar">
-            <a>Описание игры</a>
-            <a>Уровни</a>
-            <a>Тарифы</a>
-            <a>Отзывы</a>
-            <a>Партнеры</a>
-            <a>Профиль</a>
-            <a>Игра</a>
-        </div>
-    );
+     
+    </nav>
+  );
 }
-
 
 export default Navbar;
